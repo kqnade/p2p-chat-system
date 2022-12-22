6a3671161cfcc644646aba5df38e8b08a50ae1b1
K4nade <96651374+kqnade@users.noreply.github.com>
2022-12-22 23:47:22 +0900

    first commit

---
+++ script.py
@@ -0,0 +1,10 @@
+import os
+from subprocess import call
+from subprocess import Popen
+import time
+
+for i in range(4):
+    command = 'python main.py 889{} id{}'.format(i + 1, i + 1)
+    # can't create peers too fast. 
+    time.sleep(0.2)
+    Popen(['xterm', '-e', command])
